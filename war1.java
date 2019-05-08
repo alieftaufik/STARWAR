c063c9c7757298acf177d9040b319b35681a7db2
ALIEF TAUFIK <43697032+alieftaufik@users.noreply.github.com>
2019-05-08 07:03:27 +0700

    Create war1.java

---
+++ war1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class war1 {
+
+	public static void main(String[] args) {
+		
+		Scanner a = new Scanner(System.in);
+		System.out.print("Input angka : ");
+		int batas = a.nextInt();
+		for (int i = 0; i < batas+2; i++) {
+			
+				int sisa = (i) % 2;
+				if (sisa == 0) {
+					System.out.print(i+" ");
+				}
+			}
+	}
+}
