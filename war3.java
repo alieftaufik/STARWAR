1abd8e279f276f358d4ad52bcdfed46bdb70f1fd
ALIEF TAUFIK <43697032+alieftaufik@users.noreply.github.com>
2019-05-10 12:36:25 +0700

    Create war3.java

---
+++ war3.java
@@ -0,0 +1,17 @@
+package starwar;
+
+import java.util.Scanner;
+
+public class war3 {
+
+	public static void main(String[] args) {
+			
+			Scanner a = new Scanner(System.in);
+			System.out.println("INPUT");
+			int batas = a.nextInt();
+			for (int i = 1; i < batas+1; i++) {
+						System.out.println("OUTPUT");
+						System.out.print(i+" ");
+				}
+			}
+	}
