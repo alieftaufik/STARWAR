41501d941621da15aebfc5588cad100e91bd616c
ALIEF TAUFIK <43697032+alieftaufik@users.noreply.github.com>
2019-05-10 12:22:08 +0700

    Update war1.java

--- war1.java
+++ war1.java
@@ -5,12 +5,13 @@ public class war1 {
 	public static void main(String[] args) {
 		
 		Scanner a = new Scanner(System.in);
-		System.out.print("Input angka : ");
+		System.out.print("INPUT");
 		int batas = a.nextInt();
 		for (int i = 0; i < batas+2; i++) {
 			
 				int sisa = (i) % 2;
 				if (sisa == 0) {
+					System.out.print("OUTPUT");
 					System.out.print(i+" ");
 				}
 			}
