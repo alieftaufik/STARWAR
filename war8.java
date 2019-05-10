33310fa567ea6178c6787c6e1d0dc318d1b4db35
ALIEF TAUFIK <43697032+alieftaufik@users.noreply.github.com>
2019-05-10 13:00:26 +0700

    Create war8.java

---
+++ war8.java
@@ -0,0 +1,19 @@
+package starwar;
+import java.util.Scanner;
+
+public class war8 {
+	    public static void main(String[] args) {
+	       
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("INPUT");
+	        int a = sc.nextInt();
+	        System.out.println("OUTPUT");
+	        for (int i = 0; i < a; i++) {
+	            for (int k = 0; k < (a-i); k++) {
+	                System.out.print("X ");
+	            }
+	            System.out.println();
+	        }
+    }
+}
+
